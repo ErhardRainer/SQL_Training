33142b6e1ef4575f627f5d4668534d53bb5161fe
ErhardRainer <95591871+ErhardRainer@users.noreply.github.com>
2025-09-06 02:54:51 +0200

    Create Themen.js

---
+++ T-SQL/Themen.js
@@ -0,0 +1,12 @@
+<!-- in einer .md auf GitHub Pages funktioniert HTML; in README nicht -->
+<table class="sortable">
+  <thead>
+    <tr><th>Name</th><th>Punkte</th><th>Datum</th></tr>
+  </thead>
+  <tbody>
+    <tr><td>A</td><td>12</td><td>2025-09-06</td></tr>
+    <tr><td>B</td><td>7</td><td>2025-09-01</td></tr>
+    <tr><td>C</td><td>19</td><td>2025-08-30</td></tr>
+  </tbody>
+</table>
+<script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
